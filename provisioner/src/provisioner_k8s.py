411457962648b3c9e627068c233642a345793a8f
Igor Sfiligoi <isfiligoi@sdsc.edu>
2021-09-20 19:28:54 +0000

    Add authenticate method

--- provisioner/src/provisioner_k8s.py
+++ provisioner/src/provisioner_k8s.py
@@ -10,6 +10,7 @@ import copy
 import re
 import kubernetes
 
+
 class ProvisionerK8S:
    """Kubernetes Query interface"""
 
@@ -20,6 +21,17 @@ class ProvisionerK8S:
              Monitored namespace
       """
       self.namespace = copy.deepcopy(namespace)
+
+   def authenticate(self, use_service_account=True):
+      """Load credentials needed for authentication"""
+      if use_service_account:
+         # The pod must have a service accoun associated with it
+         # https://kubernetes.io/docs/tasks/configure-pod-container/configure-service-account/
+         # It also relies on KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT beign set
+         kubernetes.config.load_incluster_config()
+      else:
+         # Read config from ~/.kube/config
+         kubernetes.config.load_kube_config()
 
    def query_idle(self):
       """Return the list of idle jobs for my namespace"""
@@ -37,57 +49,4 @@ class ProvisionerK8S:
       # TBD: We may need to check for continuation flag
       return jobjs.items:
 
-class ProvisionerClusteredK8S:
-   """Kubernetes Query interface with clustering"""
-
-   def __init__(self, namespace):
-      """
-      Arguments:
-         namespace: string
-             Monitored namespace
-      """
-      self.k8s=ProvisionerK8S(namespace)
-
-      # fix list of attributes and their defaults here
-      self.int_attrs={'RequestCpus':1,
-                      'RequestMemory':1024,
-                      'RequestDisk':100000,
-                      'RequestGPUs':0 }
-
-   def query_idle(self):
-      """Return the number of idle jobs by cluster for my namespace"""
-      return self.query(job_status=1)
-
-
-   def query(self, job_status):
-      """Return the number of jobs by cluster for my namespace"""
-
-      clusters={}
-      jobs=self.schedd.query(job_status=job_status, projection=list(self.int_attrs.keys()))
-      for job in jobs:
-      return jobjs.items:
-         try:
-            specs=j.spec.template.spec.containers[0].resources.requests
-         except:
-            continue # ignore malformed jobs
-         jobs
-         cluster_key=[]
-         cluster_id={}
-         for attr in self.int_attrs:
-            try:
-               val=job[attr]
-               if type(val)==classad.classad.ExprTree:
-                  val=val.eval()
-               val=int(val)
-            except:
-               # if anything goes wrong, use default value
-               val=self.int_attrs[attr]
-            cluster_id[attr]=val
-            cluster_key.append(val)
-
-         cluster_key=tuple(cluster_key)
-         if cluster_key not in clusters:
-            clusters[cluster_key]={'full':cluster_id,'value':0}
-         clusters[cluster_key]['value']+=1
-      return clusters
 
