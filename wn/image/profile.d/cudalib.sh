3e955476c097a5e4b5373bc69d010d4b74bb0b88
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-02-09 10:17:15 -0800

    Add path to cudalib.sh

--- wn/image/profile.d/cudalib.sh
+++ wn/image/profile.d/cudalib.sh
@@ -1,1 +1,2 @@
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib64:/usr/local/cuda-11/targets/x86_64-linux/lib
+export PATH=${PATH}:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/bin:/usr/bin:/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib64:/usr/local/cuda/lib64/:/usr/local/nvidia/lib64/
