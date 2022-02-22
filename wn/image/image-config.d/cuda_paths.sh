0b6eedfebf8cd1eebae757c729afb227027a0770
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-02-22 15:20:28 -0800

    Add image-config.d/cuda_paths.sh

---
+++ wn/image/image-config.d/cuda_paths.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "x${NVIDIA_SYMLINK}" == "xyes" ]; then
+  # some environments have NVIDIA tools in non-standard locations
+
+  for d in /usr/local/nvidia/bin 
+  do
+    pushd $d
+    for f in *; do
+      ln -s $d/$f /bin/$f
+    done
+    popd
+  done
+
+  for d in /usr/local/nvidia/lib64 /usr/local/cuda/lib64
+  do
+    pushd $d
+    for f in *so*; do
+      ln -s $d/$f /usr/lib64/$f
+    done
+    popd
+  done
+fi
+
+exit 0
