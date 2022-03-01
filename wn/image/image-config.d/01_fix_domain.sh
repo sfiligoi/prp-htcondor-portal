bfb74be5b4de5f66786f28c0feb9c9d335013db7
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-02-28 19:05:59 -0800

    Fix typos

--- wn/image/image-config.d/01_fix_domain.sh
+++ wn/image/image-config.d/01_fix_domain.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ 'x${K8S_NAMESPACE}' == 'x' ]; then
-  K8S_NAMESPACE='prp'
+if [ "x${K8S_NAMESPACE}" == "x" ]; then
+  K8S_NAMESPACE=prp
 fi
-if [ 'x${PHYSICAL_HOSTNAME}' == 'x' ]; then
-  PHYSICAL_HOSTNAME='me'
+if [ "x${PHYSICAL_HOSTNAME}" == "x" ]; then
+  PHYSICAL_HOSTNAME=me
 fi
-if [ 'x${K8S_DOMAIN}' == 'x' ]; then
-  K8S_DOMAIN='optiputer.net'
+if [ "x${K8S_DOMAIN}" == "x" ]; then
+  K8S_DOMAIN=optiputer.net
 fi
 
 # k8s nodes have no domain, which is annoying
 # Add it here
-cp /etc/hosts /tmp/hosts && sed "s/\(osg-wn-.*\)/\1\.${PHYSICAL_HOSTNAME}.${K8S_NAMESPACE}.${K8S_DOMAIN} \1/" /tmp/hosts > /etc/hosts && rm -f /tmp/hosts
+cp /etc/hosts /tmp/hosts && sed "s/\(${HOSTNAME}\/\1\.${PHYSICAL_HOSTNAME}.${K8S_NAMESPACE}.${K8S_DOMAIN} \1/" /tmp/hosts > /etc/hosts && rm -f /tmp/hosts
