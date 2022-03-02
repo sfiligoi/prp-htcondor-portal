c2c55ea40e15d0a1f6c12d42b91aa2a2ac22f9fa
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-03-01 17:02:49 -0800

    Remove . from PHYSICAL_HOSTNAME section of the domain

--- wn/image/image-config.d/01_fix_domain.sh
+++ wn/image/image-config.d/01_fix_domain.sh
@@ -10,6 +10,9 @@ if [ "x${K8S_DOMAIN}" == "x" ]; then
   K8S_DOMAIN=optiputer.net
 fi
 
+# let's keep . for separators only
+hstr=`echo "${PHYSICAL_HOSTNAME}" | tr '.' '-'`
+
 # k8s nodes have no domain, which is annoying
 # Add it here
-cp /etc/hosts /tmp/hosts && sed "s/\(${HOSTNAME}\)/\1\.${PHYSICAL_HOSTNAME}.${K8S_NAMESPACE}.${K8S_DOMAIN} \1/" /tmp/hosts > /etc/hosts && rm -f /tmp/hosts
+cp /etc/hosts /tmp/hosts && sed "s/\(${HOSTNAME}\)/\1\.${hstr}.${K8S_NAMESPACE}.${K8S_DOMAIN} \1/" /tmp/hosts > /etc/hosts && rm -f /tmp/hosts
